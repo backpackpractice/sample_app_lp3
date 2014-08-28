2285d752c6b98dfeb1af45dfb6c2b0ad55ff31d7
Joe Wagner <jwagner@backpackpractice.org>
2014-08-28 14:58:36 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   get "static_pages/home"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
@@ -10,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root  'static_pages#home'
+  #root  'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
